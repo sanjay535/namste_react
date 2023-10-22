eb5c4b6b29640974997b3226d98193ca261fa0e9
Sanjay Gautam <sanjaykumargautam535@gmail.com>
2023-10-22 22:33:01 +0530

    Lazy loaded Contact component

--- app.js
+++ app.js
@@ -18,16 +18,19 @@ import ReactDOM from 'react-dom/client';
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import AppLayout from './src/components/AppLayout';
 import About from './src/pages/About';
-import Contact from './src/pages/Contact';
+// import Contact from './src/pages/Contact';
 import Body from './src/components/Body';
 import Login from './src/pages/Login';
 import Profile from './src/components/Profile.js';
 import ClassComponent from './src/components/ClassTest';
+import { lazy, Suspense } from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 //   const p1=React.createElement('p', {id:"p1", key:1}, "React Tutorial");
 //   const p2=React.createElement('p', {id:"p2", key:2}, "It's all anbout React");
 //   const container=React.createElement('div', {className:"Container"}, [p1, p2]);
+
+const Contact=lazy(()=>import('./src/pages/Contact'));
 const appRouter=createBrowserRouter([
   {
     path:'/',
@@ -54,7 +57,7 @@ const appRouter=createBrowserRouter([
       },
       {
         path:'/contact',
-        element:<Contact/>
+        element:<Suspense fallback={<>Loading...</>}><Contact/></Suspense>
       },
       {
         path:'/login',
